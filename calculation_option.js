c893c3179c75d811feabaaf8c53421ce2a7bafdd
DrakeAndrewScorpious <75718164+DrakeAndrewScorpious@users.noreply.github.com>
2021-02-28 17:59:21 +0530

    Add files via upload

---
+++ calculation_option.js
@@ -0,0 +1,15 @@
+function addition() {
+    window.location = "addition.html";
+}
+
+function subtraction() {
+    window.location = "subtraction.html";
+}
+
+function multiplication() {
+    window.location = "multiplication.html";
+}
+
+function division() {
+    window.location = "division.html";
+}
